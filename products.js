a5294033c228cad37774d9bae813912c3579fbd2
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-10-03 18:02:54 +0530

    Modification Done

--- products.js
+++ products.js
@@ -36,17 +36,119 @@ var products = [
     os: "Windows",
   },
 ];
-var copyArr = [];
+
+let copyArr = [];
 for (i = 0; i < products.length; i++) {
   copyArr[i] = products[i];
 }
-console.log(copyArr);
 
 $(document).ready(function () {
   displayCopy();
+
+  $("#filter").click(function () {
+    let table =
+      "<table border=1px><tr><th>ID</th><th>Name</th><th>Brand</th><th>Operating System</th><th>Remove</th></tr>";
+    var OS = document.getElementById("OS").value;
+    var Brand = document.getElementById("Brand").value;
+
+    products.forEach((element) => {
+      if (element.os == OS) {
+        table +=
+          "<tr><td>" +
+          element.id +
+          "</td><td>" +
+          element.name +
+          "</td><td>" +
+          element.brand +
+          "</td><td>" +
+          element.os +
+          "</td><td>" +
+          "<p  onclick=\"delProd('" +
+          element.id +
+          "')\">X</p>" +
+          "</td><tr>";
+      } else if (element.brand == Brand) {
+        table +=
+          "<tr><td>" +
+          element.id +
+          "</td><td>" +
+          element.name +
+          "</td><td>" +
+          element.brand +
+          "</td><td>" +
+          element.os +
+          "</td><td>" +
+          "<p onclick=\"delProd('" +
+          element.id +
+          "')\">X</p>" +
+          "</td><tr>";
+      } else if (element.os == OS && element.brand == Brand) {
+        table +=
+          "<tr><td>" +
+          element.id +
+          "</td><td>" +
+          element.name +
+          "</td><td>" +
+          element.brand +
+          "</td><td>" +
+          element.os +
+          "</td><td>" +
+          "<p onclick=\"delProd('" +
+          element.id +
+          "')\">X</p>" +
+          "</td><tr>";
+      }
+    });
+
+    table += "</table>";
+    document.getElementById("output").innerHTML = table;
+  });
+
+  $("#searchEngine").click(function () {
+    let table =
+      "<table border=1px><tr><th>ID</th><th>Name</th><th>Brand</th><th>Operating System</th><th>Remove</th></tr>";
+    var tval = document.getElementById("search").value;
+    tval = tval.toLowerCase();
+    products.forEach((element) => {
+      if (element.name.toLowerCase() == tval) {
+        table +=
+          "<tr><td>" +
+          element.id +
+          "</td><td>" +
+          element.name +
+          "</td><td>" +
+          element.brand +
+          "</td><td>" +
+          element.os +
+          "</td><td>" +
+          "<p onclick=\"delProd('" +
+          element.id +
+          "')\">X</p>" +
+          "</td><tr>";
+      } else if (element.id == tval) {
+        table +=
+          "<tr><td>" +
+          element.id +
+          "</td><td>" +
+          element.name +
+          "</td><td>" +
+          element.brand +
+          "</td><td>" +
+          element.os +
+          "</td><td>" +
+          "<p onclick=\"delProd('" +
+          element.id +
+          "')\">X</p>" +
+          "</td><tr>";
+      }
+    });
+
+    table += "</table>";
+    document.getElementById("res").innerHTML = table;
+  });
 });
 
-function displayCopy() {
+const displayCopy = () => {
   var table =
     "<table border=1px><tr><th>ID</th><th>Name</th><th>Brand</th><th>Operating System</th><th>Remove</th></tr>";
   copyArr.forEach((element) => {
@@ -68,109 +170,7 @@ function displayCopy() {
 
   table += "</table>";
   document.getElementById("output").innerHTML = table;
-}
-
-function filter() {
-  let table =
-    "<table border=1px><tr><th>ID</th><th>Name</th><th>Brand</th><th>Operating System</th><th>Remove</th></tr>";
-  var OS = document.getElementById("OS").value;
-  var Brand = document.getElementById("Brand").value;
-
-  products.forEach((element) => {
-    if (element.os == OS) {
-      table +=
-        "<tr><td>" +
-        element.id +
-        "</td><td>" +
-        element.name +
-        "</td><td>" +
-        element.brand +
-        "</td><td>" +
-        element.os +
-        "</td><td>" +
-        "<p  onclick=\"delProd('" +
-        element.id +
-        "')\">X</p>" +
-        "</td><tr>";
-    } else if (element.brand == Brand) {
-      table +=
-        "<tr><td>" +
-        element.id +
-        "</td><td>" +
-        element.name +
-        "</td><td>" +
-        element.brand +
-        "</td><td>" +
-        element.os +
-        "</td><td>" +
-        "<p onclick=\"delProd('" +
-        element.id +
-        "')\">X</p>" +
-        "</td><tr>";
-    } else if (element.os == OS && element.brand == Brand) {
-      table +=
-        "<tr><td>" +
-        element.id +
-        "</td><td>" +
-        element.name +
-        "</td><td>" +
-        element.brand +
-        "</td><td>" +
-        element.os +
-        "</td><td>" +
-        "<p onclick=\"delProd('" +
-        element.id +
-        "')\">X</p>" +
-        "</td><tr>";
-    }
-  });
-
-  table += "</table>";
-  document.getElementById("output").innerHTML = table;
-}
-
-function searchData() {
-  let table =
-    "<table border=1px><tr><th>ID</th><th>Name</th><th>Brand</th><th>Operating System</th><th>Remove</th></tr>";
-  var tval = document.getElementById("search").value;
-  tval = tval.toLowerCase();
-  products.forEach((element) => {
-    if (element.name.toLowerCase() == tval) {
-      table +=
-        "<tr><td>" +
-        element.id +
-        "</td><td>" +
-        element.name +
-        "</td><td>" +
-        element.brand +
-        "</td><td>" +
-        element.os +
-        "</td><td>" +
-        "<p onclick=\"delProd('" +
-        element.id +
-        "')\">X</p>" +
-        "</td><tr>";
-    } else if (element.id == tval) {
-      table +=
-        "<tr><td>" +
-        element.id +
-        "</td><td>" +
-        element.name +
-        "</td><td>" +
-        element.brand +
-        "</td><td>" +
-        element.os +
-        "</td><td>" +
-        "<p onclick=\"delProd('" +
-        element.id +
-        "')\">X</p>" +
-        "</td><tr>";
-    }
-  });
-
-  table += "</table>";
-  document.getElementById("output").innerHTML = table;
-}
+};
 
 function delProd(val) {
   for (let i = 0; i < copyArr.length; i++) {
